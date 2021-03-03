3b01ec7e97c6d55f96ee12b3a0aaad4bfea6ec2c
stevepae <stevepaemusic@gmail.com>
2021-03-02 21:30:06 -0500

    added modal cards with data

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 // IPO Pattern for program design - Input -> Process -> Output
 
 // constants
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon ";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 
 // apps state (variables)
+let pokemonData;
+
+
 // cached element references
+const $collection = $('#collection');
+const $modal = $('.modal');
+const $name = $('#name');
+const $moves = $('#moves');
+const $height = $('#height');
+const $forms = $('#forms');
+const $sprite = $('#sprite');
+
+
 // event listeners
+$collection.on('click', 'article.card', handleClick);
+
 // functions
 
 // execute all initial actions when page loads (optional)
@@ -16,12 +30,45 @@ function init() {
     getData();
 }
 
+function handleClick(evt) {
+    $.ajax(this.dataset.url)
+    .then(function(data) {
+$name.text(data.name).css('text-transform', 'capitalize');
+$moves.text("Moves: " + data.moves.length);
+$height.text("Height: " + data.height);
+$forms.text("Forms: " + data.forms.length);
+$sprite.attr({src: data.sprites.front_default, alt: data.name});
+        console.log(data)
+        $modal.modal();
+    }, function(error) {
+        console.log(error)
+    })
+    console.log(this.dataset.url);
+}
+
 
 function getData() {
-    $.ajax(BASE_URL)
+    $.ajax(BASE_URL + "?limit=30")
     .then(function(data) {
         console.log('data: ', data);
+        // asssign data to globally accessible variable
+        pokemonData = data;
+        // update Dom with the Data
+        render();
     }, function(error) {
         console.log('error: ', error);
     });
 }
+function render() {
+    
+    const cards = pokemonData.results.map(function(pokemon) {
+        return `
+        <article data-url="${pokemon.url}" class=" card">
+        <h3>${pokemon.name}</h3>
+    </article> 
+    `;
+    })
+    $collection.html(cards);
+}
+
+
